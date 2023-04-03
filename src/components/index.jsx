b7dc2313b54bf25cacef742f0e53ee38db90ac79
Darwin Roa <darwinmichaelroamora@gmail.com>
2023-04-02 23:46:39 -0500

    agregando / a la base url

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import store from "../redux/store";
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://backend-del-hotel-production-c60b.up.railway.app';
+axios.defaults.baseURL = 'https://backend-del-hotel-production-c60b.up.railway.app/';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
